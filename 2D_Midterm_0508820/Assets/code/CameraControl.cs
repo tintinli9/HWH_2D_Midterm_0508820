b2c6d87c6399ee93f4e8bc61af9147e5fb5b99df
tintinli9 <71861182+tintinli9@users.noreply.github.com>
2021-04-23 00:11:30 +0800

    FIN

---
+++ 2D_Midterm_0508820/Assets/code/CameraControl.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("追蹤速度"), Range(0, 50)]
+    public float speed = 1.5f;
+    [Header("左右邊界")]
+    public Vector2 limitX = new Vector2(-5, 5);
+}
--- 2D_Midterm_0508820/Assets/code/Player.cs
+++ 2D_Midterm_0508820/Assets/code/Player.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int lv = 1;
+    [Range(0,15)]
+    public float speed = 10.5f;
+    public bool isDEAD = false;
+    public string cNAME = "主角";
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("變形元件")]
+    public Transform tra;
+    [Header("動畫元件")]
+    public Animator ani;
+
+    private void Move()
+    {
+        float h = joystick.Horizontal;
+
+        tra.Translate(h * speed * Time.deltaTime, 0, 0);
+
+        ani.SetFloat("水平", h);
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void Hit()
+    {
+
+    }
+
+    private void Dead()
+    {
+
+    }
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
     }
 }
+
